110fa349284c19b50977a669544834afcbe291dd
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-08-09 17:03:20 -0500

    Clean up welcome screen test

--- test/welcome_screen_test.dart
+++ test/welcome_screen_test.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_sample_app/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_sample_app/screens/welcome_screen.dart';
 
 void main() {
-  testWidgets('Renders WelcomeScreen and starts flow on button press',
+  testWidgets('Renders WelcomeScreen message and button',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: WelcomeScreen()));
+    await tester.pumpWidget(const MyApp());
 
     expect(
         find.text("Welcome to Descope's Flutter Sample App!"), findsOneWidget);
     expect(find.byType(CupertinoButton), findsOneWidget);
-
-    // Simulate button press to start the flow
-    // Additional logic to check the flow and navigation to HomeScreen should be added if needed
   });
 }
